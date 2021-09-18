310d2fb8c2fc30c0727e409994cdffbc074ab764
Bruno Rodrigues <brunorodrigues355@gmail.com>
2021-09-18 09:58:42 +0100

    feat: Internationalization config with ssr

---
+++ src/routes/[lang]/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export async function load({ page, fetch, session, context }) {
+		const lang = page.params.lang;
+
+		// Get homepage and menu content from api according to lang
+
+		if (lang) {
+			// Return homepage content
+			return {
+				props: {
+					lang
+				}
+			};
+		}
+	}
+</script>
+
+<script>
+	export let lang;
+</script>
+
+<h1>Home</h1>
+<p>{lang}</p>
