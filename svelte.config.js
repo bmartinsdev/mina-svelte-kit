d7775419d350d2ec93131c177060497d8fe014db
Bruno Rodrigues <brunorodrigues355@gmail.com>
2021-09-19 12:38:38 +0100

    feat: Setup contentful env and packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,25 @@
 // svelte.config.js
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node'
+import 'dotenv/config'
+import replace from '@rollup/plugin-replace'
+import preprocess from 'svelte-preprocess'
+
+// Get env keys from .env file
+const keys = [
+	`CONTENTFUL_ACCESS_TOKEN`,
+	`CONTENTFUL_SPACE_ID`,
+]
+const replacements = Object.fromEntries(
+	keys.map((key) => [`process.env.${key}`, JSON.stringify(process.env[key])])
+)
 
 export default {
+	preprocess: preprocess(),
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			out: 'build',
-			precompress: false,
-			env: {
-				host: 'HOST',
-				port: 'PORT'
-			}
-		})
+		adapter: adapter(),
+		target: `#svelte`,
+		vite: {
+			plugins: [replace(replacements)],
+		},
 	}
 };
